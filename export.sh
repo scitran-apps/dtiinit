fca023b5a51407fe2e29ec91ea9579e1c3bef423
Michael Perry <lmperry@stanford.edu>
2016-09-22 10:22:33 -0700

    v0.1.1: Accomodate v1 flywheel gear spec for input directories

--- export.sh
+++ export.sh
@@ -2,7 +2,7 @@
 # Exports the container in the cwd.
 # The container can be exported once it's started with
 gear=dtiinit
-version=0.1.0
+version=0.1.1
 outname=$gear-$version.tar
 container=$gear
 image=scitran/$gear
