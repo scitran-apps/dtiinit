4848cf755c7d97d062ea05f47209a5ae8730f5d5
Michael Perry <lmperry@stanford.edu>
2017-04-06 08:15:03 -0700

    Functionality to select an anatomical image for alignment

--- source/bin/parse_config.py
+++ source/bin/parse_config.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Parse a config file and create a dtiInit params json file.
-def parse_config(input_file, output_file, input_dir, output_dir, nifti_dir, bvec_dir, bval_dir):
+def parse_config(input_file, output_file, input_dir, output_dir, nifti_dir, bvec_dir, bval_dir, anat_dir):
     import os
     import json
     import glob
@@ -42,6 +42,13 @@ def parse_config(input_file, output_file, input_dir, output_dir, nifti_dir, bvec
     config['input_dir']  = input_dir
     config['output_dir'] = output_dir
 
+    # Add anatomical file for dtiInit
+    if os.path.exists(anat_dir) and os.listdir(anat_dir):
+        config['t1_file'] = glob.glob(os.path.join(anat_dir, '*.nii.gz'))[0]
+    else:
+        config['t1_file'] = '/templates/MNI_EPI.nii.gz'
+
+
     # Rename the bval and bvec files to match the dti file to deal with how the
     # matlab code combines related files
     config['bval_file'] = glob.glob(os.path.join(bval_dir, '*.bval*'))[0]
@@ -77,12 +84,13 @@ if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument('--input_file', default='/flwywheel/v0/config.json', help='Full path to the input file.')
-    ap.add_argument('--output_file', default='/flywheel/v0/json/dtiinit_params.json', help='Full path to the output file.')
+    ap.add_argument('--output_file', default='/flywheel/v0/dtiInit.json', help='Full path to the output file.')
     ap.add_argument('--input_dir', default='/flwywheel/v0/input', help='Full path to the input file.')
     ap.add_argument('--output_dir', default='/flywheel/v0/output', help='Full path to the output file.')
     ap.add_argument('--bvec_dir', default='/flywheel/v0/input/bvec', help='Full path to the bvec directory.')
     ap.add_argument('--bval_dir', default='/flywheel/v0/input/bval', help='Full path to the bval directory.')
-    ap.add_argument('--nifti_dir', default='/flywheel/v0/input/nifti', help='Full path to the nifti directory.')
+    ap.add_argument('--nifti_dir', default='/flywheel/v0/input/dwi', help='Full path to the nifti directory.')
+    ap.add_argument('--anat_dir', default='/flywheel/v0/input/anatomical', help='Full path to the anatomical directory.')
     args = ap.parse_args()
 
-    parse_config(args.input_file, args.output_file, args.input_dir, args.output_dir, args.nifti_dir, args.bvec_dir, args.bval_dir)
+    parse_config(args.input_file, args.output_file, args.input_dir, args.output_dir, args.nifti_dir, args.bvec_dir, args.bval_dir, args.anat_dir)
